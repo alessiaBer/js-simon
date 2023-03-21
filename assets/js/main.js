8e0e8d5e5028d8a97b91daa4fecf0ef8060c5f12
alessiaBer <alexdeyn@gmail.com>
2023-03-21 18:33:15 +0100

    [add] timeouts

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,9 @@ const container_el = document.querySelector('.container');
 const array_container = document.createElement('span');
 
 //assegno l'array dei numeri ad una variabile
-const numb_to_remember = [1, 2, 3, 7, 8];
+const numb_to_remember = [];
 //genero 5 numeri casuali e li inserisco nell'array
-/* function generateNumb () {
+function generateNumb () {
     const num_to_generate = 5;
     while (numb_to_remember.length < num_to_generate) {
         const random_number =  Math.floor(Math.random() * (100 - 1) ) + 1;
@@ -25,7 +25,7 @@ const numb_to_remember = [1, 2, 3, 7, 8];
     }
 }
 //invoco la funzione
-generateNumb();  */
+generateNumb(); 
 
 //trasformo l'array in una stringa
 const spanNumb = numb_to_remember.join(' ');
@@ -36,43 +36,55 @@ container_el.insertAdjacentElement('afterbegin', array_container);
 
 
 //parte il timer
+setTimeout(hideNumbers, 3000);
 
+function hideNumbers() {
+    array_container.classList.add('d-none');
+}
 //i numeri scompaiono
 
+setTimeout(askNumbers, 4000);
 //creo un array vuoto dove verranno salvati i numeri inseriti dall'utente 
-const resultArray = [];
-//con un loop che itera per il numero di i di numb_to_remember
-for (let i = 0; i < numb_to_remember.length; i++) {
-    //chiedo all'utente attraverso un prompt di inserire i numeri che ha visto e salvo il risultato in una variabile
-    const ask_numb = Number(prompt('inserisci i numeri che hai visto, uno alla volta'));
-    //assegno i numeri risultanti a un array
-    resultArray.push(ask_numb);
+let resultArray = [];
+
+function askNumbers() {
+    
+    //con un loop che itera per il numero di i di numb_to_remember
+    for (let i = 0; i < numb_to_remember.length; i++) {
+        //chiedo all'utente attraverso un prompt di inserire i numeri che ha visto e salvo il risultato in una variabile
+        const ask_numb = Number(prompt('inserisci i numeri che hai visto, uno alla volta'));
+        //assegno i numeri risultanti a un array
+        resultArray.push(ask_numb);
+    } 
+    
+    answer();
 }
+
 
 //console.log(resultArray);
 //console.log(numb_to_remember)
 
 //confronto l'array creato con l'array iniziale
 function answer() {
-let totNumb = 0;
-let guessedArray = [];
+    let totNumb = 0;
+    let guessedArray = [];
 
     for (let i = 0; i < numb_to_remember.length; i++) {
-    const ask_numb = resultArray[i];
-    
-    
-    
-    //se un numero dell'array creato appartiene all'array dei numeri casuali
-    if (numb_to_remember.includes(ask_numb)) {
-        //lo stampo in console
-        guessedArray.push(ask_numb);
+        const ask_numb = resultArray[i];
+        
+        //se un numero dell'array creato appartiene all'array dei numeri casuali
+        if (numb_to_remember.includes(ask_numb)) {
+            //lo aggiungo all'array dei numeri indovinati
+            guessedArray.push(ask_numb);
 
-        totNumb++;
-    }
-} 
- print(totNumb, guessedArray);
+            //incremento il valore di numeri indovinati
+            totNumb++;
+        }
+    } 
+
+    print(totNumb, guessedArray);
 }
-answer();
+
 
 function print(number, array) {
     const resultSpan = document.createElement('span');
